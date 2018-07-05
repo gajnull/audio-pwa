e416bd4b085cf58e937085b42f3fe85589e6cdc7
gajnull <gajnull@mail.ru>
2018-07-05 23:13:19 +0300

    'last'

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import files from './data/data';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+// const App2 = () => (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <h1 className="App-title">Welcome to React</h1>
+//     </header>
+//     <p className="App-intro">
+//       To get started, edit <code>src/App.js</code> and save to reload.
+//     </p>
+//   </div>
+// );
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {files};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Lngt</h1>
+        </header>
+        <ul className="App-intro">
+          { this.state.files.map((file, index) =>
+              <li key={index}>{file.name}</li>)
+          }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/data/data.js
@@ -0,0 +1,6 @@
+const files = [
+  { name: 'Dialog1', txt: 'Dialog1.lngt', audio: 'Dialog1.mp3' },
+  { name: 'Dialog2', txt: 'Dialog2.lngt', audio: 'Dialog2.mp3' }
+];
+
+export default files;
