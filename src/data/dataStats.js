64eab5f1120cc4400fff551a9bff37cee3f5f006
gajnull <gajnull@mail.ru>
2018-08-19 22:30:07 +0300

    'last'

--- src/data/dataStats.js
+++ src/data/dataStats.js
@@ -1,35 +1,65 @@
 
 const zeroStats = {
-  countRuns: 0, 
-  workingTime: 0, 
-  countDialogs: 0
+  countRuns: 0,
+  workingTime: 0,
+  countDialogs: 0,
+  dialogs: []
 };
 
 let stats = zeroStats;
 
+let startTimeApp; //
+let startTime;
+
 const getStats = () => {
-  return stats;
+  return fetchStats() || stats;
 };
 
-const setParam = (name, value) => {
-  //
+const setParam = (name) => {  // name = 'startApp'
+
+};
+
+
+const startApp = () => {
+  stats.countRuns++;
 };
 
 // Запускаем отсчёт времени работы с диалогом
-const start = (dialog) => {
-  //
+const startDialog = (dialog) => {
+  startTime = new Date();
 };
 
 // Останавливаем отсчёт времени работы с диалогом и записываем в статистику
-const stop = () => {
-  //
+const stopDialog = (dialog) => {
+  const duration = new Date() - startTime;
+  stats.workingTime += duration;
 };
 
+
+
+
+
+function saveStats() {
+  const ls = JSON.stringify(stats);
+  try {
+    localStorage.setItem('stats_lngt', ls);
+  } catch (e) {
+     console.log('settings не сохранены в localStorage');
+  }
+}
+
+function fetchStats() {
+  try {
+    return JSON.parse(localStorage.getItem('stats_lngt'));
+  } catch (e) {
+     return false;
+  }
+}
 
 
 export default {
   getStats,
-  setParam,
-  start,
-  stop
+//  setParam,
+  startDialog,
+  stopDialog
 };
