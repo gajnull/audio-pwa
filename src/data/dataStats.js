873155f465f8e44f82dfb47220ddd4faada6dc3a
Ruslan <gajnull@mail.ru>
2018-08-20 18:55:40 +0300

    'last'

--- src/data/dataStats.js
+++ src/data/dataStats.js
@@ -3,43 +3,59 @@ const zeroStats = {
   countRuns: 0,
   workingTime: 0,
   countDialogs: 0,
-  dialogs: []
+  dialogs: [] // {name, count}
 };
 
-let stats = zeroStats;
-
-let startTimeApp; //
-let startTime;
+let startDialogTime;
 
 const getStats = () => {
-  return fetchStats() || stats;
+  return fetchStats() || zeroStats;
 };
 
-const setParam = (name) => {  // name = 'startApp'
+//const setParam = (name) => {};
 
-};
-
-
+// Должно выполняться при запуске приложения
 const startApp = () => {
+  const stats = getStats();
   stats.countRuns++;
+  saveStats();
 };
 
 // Запускаем отсчёт времени работы с диалогом
 const startDialog = (dialog) => {
-  startTime = new Date();
+  startDialogTime = new Date();
 };
 
-// Останавливаем отсчёт времени работы с диалогом и записываем в статистику
+// Останавливаем отсчёт времени работы с диалогом и если работали с ним достаточно, записываем в статистику
 const stopDialog = (dialog) => {
-  const duration = new Date() - startTime;
-  stats.workingTime += duration;
+  const duration = new Date() - startDialogTime;  // в миллисекундах
+  if (duration < 20000) return; // если меньше 20 секунд, то не считается
+  recordDataDialogs(dialog, duration);
 };
 
+function recordDataDialogs(name, duration) {
+  const stats = getStats();
+  stats.workingTime += duration;
+  const dialogs = stats.dialogs
+  stats.dialogs = addInfoDialogs(name, dialogs);
+  stats.countDialogs = stats.dialogs.length;
+  saveStats(stats);
+}
+
+function addInfoDialogs(name, dialogs) {
+  for (let i = 0; i < dialogs.length; i++) {
+    if (dialogs[i].name === name) {
+      dialogs[i].count++;
+      return dialogs;
+    }
+  }
+  dialogs.push({name, count: 1});
+  return dialogs; // в пердыдущей строке нельзя return, т.к. возвращается длина массива  
+}
 
 
 
-
-function saveStats() {
+function saveStats(stats) {
   const ls = JSON.stringify(stats);
   try {
     localStorage.setItem('stats_lngt', ls);
@@ -59,7 +75,7 @@ function fetchStats() {
 
 export default {
   getStats,
-//  setParam,
+  startApp,
   startDialog,
   stopDialog
 };
